3327f7aa75a11cd3a485761a32196ea7810c62ff
Isaac <ryanwoo1989@gmail.com>
2015-01-22 00:35:00 -0500

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
-        if(target<matrix[0][0]) return false;
-        const int m=matrix.size();
-        if(m==0)    return false;
-        const int n=matrix[0].size();
-        int i;
-        for(i=0; i<m; ++i){
-            if(target==matrix[i][0])    return true;
-            if(target>matrix[i][0] && (i==m-1 || target<matrix[i+1][0]))
-                break;
+        if(matrix.empty() || target<matrix[0][0])   return false;
+        int i=0, j=matrix.size()-1;
+        while(i<j){
+            int m=(i+j)/2;
+            if(matrix[m][0]==target)    return true;
+            if(matrix[m][0]>target)
+                j=m-1;
+            else
+                i=m+1;
         }
-        for(int j=1; j<n; ++j)
-            if(target==matrix[i][j])    return true;
-            else if(target<matrix[i][j])    return false;
+        if(matrix[i][0]>target) --i;
+        int x=0, y=matrix[0].size()-1;
+        while(x<=y){
+            int m=(x+y)/2;
+            if(matrix[i][m]==target)    return true;
+            if(matrix[i][m]>target)
+                y=m-1;
+            else
+                x=m+1;
+        }
         return false;
     }
 };
