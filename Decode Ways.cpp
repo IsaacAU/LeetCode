a562b88fd4fcd224e808b596ea1cfac6a6761fd9
Isaac <ryanwoo1989@gmail.com>
2015-01-22 01:50:37 -0500

    Update Decode Ways.cpp

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -19,3 +19,26 @@ public:
         return v.back();
     }
 };
+
+
+// method two:
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s.empty() || s[0]=='0')  return 0;
+        int x=1, y=1;
+        for(int i=1; i<s.length(); ++i){
+            int yp=y;
+            if(s[i]=='0'){
+                if(s[i-1]=='0' || s[i-1]>'2')
+                    return 0;
+                else{
+                    y=x;
+                }
+            }else if(s[i-1]=='1' || s[i-1]=='2' && s[i]<='6')
+                    y+=x;
+            x=yp;
+        }
+        return y;
+    }
+};
