ca0224f1b6ca7eb02e240d8a1c9a53d530fc6e3b
Isaac <ryanwoo1989@gmail.com>
2015-01-25 13:45:19 -0500

    Update Unique Binary Search Trees.cpp

--- codes/Unique Binary Search Trees.cpp
+++ codes/Unique Binary Search Trees.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // recursion
     int numTrees(int n) {
         if(n<2) return 1;
         int res=0;
@@ -7,5 +8,15 @@ public:
             res+=numTrees(i)*numTrees(n-1-i);
         }
         return res;
+    }
+    // DP
+    int numTrees(int n) {
+        if(n<=2)    return n;
+        vector<int> v(n,0);
+        v[0]=1;  v[1]=2;
+        for(int i=2; i<n; ++i)
+            for(int j=0; j<=i; ++j)
+                v[i]+=(j==0?1:v[j-1])*(j==i?1:v[i-1-j]);
+        return v.back();
     }
 };
