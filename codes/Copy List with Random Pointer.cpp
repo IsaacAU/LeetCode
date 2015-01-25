f6b6565fd281c1ef8649d0ea8739c16f990ef390
Isaac <ryanwoo1989@gmail.com>
2015-01-25 02:40:48 -0500

    Update Copy List with Random Pointer.cpp

--- codes/Copy List with Random Pointer.cpp
+++ codes/Copy List with Random Pointer.cpp
@@ -8,6 +8,7 @@
  */
 class Solution {
 public:
+    // method 1
     RandomListNode *copyRandomList(RandomListNode *head) {
         if(!head)   return nullptr;
         RandomListNode *nd=head, *copy=new RandomListNode(head->label);
@@ -25,5 +26,31 @@ public:
                 it->second->random = m[it->first->random];
         }
         return copy;
+    }
+    // method 2
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        RandomListNode *nd=head;
+        while(nd){
+            RandomListNode *tmp=nd->next;
+            nd->next=new RandomListNode(nd->label);
+            nd->next->next=tmp;
+            nd=tmp;
+        }
+        nd=head;
+        while(nd){
+            if(nd->random)
+                nd->next->random=nd->random->next;
+            nd=nd->next->next;
+        }
+        RandomListNode dummy(-1);
+        RandomListNode *prev=&dummy;
+        nd=head;
+        while(nd){
+            prev->next=nd->next;
+            nd->next=nd->next->next;
+            prev=prev->next;
+            nd=nd->next;
+        }
+        return dummy.next;
     }
 };
