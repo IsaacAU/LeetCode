fdb32420c8c563da5742e287bb24a8fe27cb4380
Isaac <ryanwoo1989@gmail.com>
2015-01-24 23:59:27 -0500

    Update Add Two Numbers.cpp

--- codes/Add Two Numbers.cpp
+++ codes/Add Two Numbers.cpp
@@ -9,12 +9,10 @@
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        if(!l1) return l2;
-        if(!l2) return l1;
         int c=0;
         ListNode dummy(-1);
-        ListNode *nd=&dummy;
-        while(l1 || l2){
+        ListNode *prev=&dummy;
+        while(l1 || l2 || c){
             int x=0, y=0;
             if(l1){
                 x=l1->val;
@@ -24,12 +22,10 @@ public:
                 y=l2->val;
                 l2=l2->next;
             }
-            nd->next = new ListNode((x+y+c)%10);
+            prev->next=new ListNode((x+y+c)%10);
+            prev=prev->next;
             c=(x+y+c)/10;
-            nd=nd->next;
         }
-        if(c)
-            nd->next = new ListNode(1);
         return dummy.next;
     }
 };
