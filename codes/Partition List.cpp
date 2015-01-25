62d59c227348e5b17069ab5f18f5ca6d8242c3eb
Isaac <ryanwoo1989@gmail.com>
2015-01-25 02:21:39 -0500

    Update Partition List.cpp

--- codes/Partition List.cpp
+++ codes/Partition List.cpp
@@ -9,31 +9,27 @@
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
-        if(!head || !head->next)    return head;
-        ListNode *pre1=nullptr, *cur1=head;
-        while(cur1 && cur1->val<x){
-            pre1=cur1;
-            cur1=cur1->next;
-        }
-        if(!cur1)   return head;
-        ListNode *pre2=cur1, *cur2=cur1->next;
-        while(cur2){
-            if(cur2->val<x){
-                pre2->next=cur2->next;
-                cur2->next=cur1;
-                if(pre1){
-                    pre1->next=cur2;
-                    pre1=pre1->next;
+        ListNode dummy(-1); dummy.next=head;
+        ListNode *prev=&dummy, *pivot=&dummy;
+        while(head){
+            if(head->val<x){
+                if(pivot->next==head){
+                    pivot=head;
+                    prev=head;
+                    head=head->next;
                 }else{
-                    head=cur2;
-                    pre1=head;
+                    prev->next=head->next;
+                    ListNode *tmp=pivot->next;
+                    pivot->next=head;
+                    head->next=tmp;
+                    pivot=head;
+                    head=prev->next;
                 }
-                cur2=pre2->next;
             }else{
-                pre2=cur2;
-                cur2=cur2->next;
+                prev=head;
+                head=head->next;
             }
         }
-        return head;
+        return dummy.next;
     }
 };
