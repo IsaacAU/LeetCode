f154453309e159f23ca424b2f7032b8d42e961bc
Isaac <ryanwoo1989@gmail.com>
2015-01-27 17:01:28 -0500

    Update Palindrome Number.cpp

--- codes/Palindrome Number.cpp
+++ codes/Palindrome Number.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
-        int n=0, y=x;
+        int y=x,z=0;
         while(y){
+            z=10*z+y%10;
             y/=10;
-            ++n;
         }
-        y=pow(10,n-1);
-        while(x){
-            if(x/y!=x%10)  return false;
-            x%=y;
-            x/=10;
-            y/=100;
-        }
-        return true;
+        return x==z;
     }
 };
