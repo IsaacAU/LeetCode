8abb25273c23346f27ce74b388068fa5169b0b22
Isaac <ryanwoo1989@gmail.com>
2015-01-27 17:50:19 -0500

    Update Search Insert Position.cpp

--- codes/Search Insert Position.cpp
+++ codes/Search Insert Position.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        int i;
-        for(i=0; i<n; ++i)
-            if(A[i]>=target)    return i;
-        return n;
+        int i=0, j=n;
+        while(i<j){
+            int m=(i+j)/2;
+            if(target==A[m])    return m;
+            if(target>A[m])
+                i=m+1;
+            else
+                j=m;
+        }
+        return i;
     }
 };
