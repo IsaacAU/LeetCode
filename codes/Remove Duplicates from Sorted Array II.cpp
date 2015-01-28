08c376e43b855e63675a360bc94766b3625f50f8
Isaac <ryanwoo1989@gmail.com>
2015-01-27 23:25:01 -0500

    Update Remove Duplicates from Sorted Array II.cpp

--- codes/Remove Duplicates from Sorted Array II.cpp
+++ codes/Remove Duplicates from Sorted Array II.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
-        int pos=-1, cur=A[0], cnt=1;
-        for(int i=1; i<n; ++i){
-            if(A[i]==cur)
-                ++cnt;
-            else{
-                cur=A[i];
-                cnt=1;
-            }
-            if(pos>=0 && cnt<3)
-                A[pos++]=A[i];
-            if(cnt==3 && pos<0)
-                pos=i;
+        if(n<=2)    return n;
+        int cnt=2, pre1=A[0], pre2=A[1];
+        for(int i=2; i<n; ++i){
+            int t1=A[i-1];
+            if(A[i]!=pre1 || A[i]!=pre2)
+                A[cnt++]=A[i];
+            pre1=t1; pre2=A[i];
         }
-        return pos<0?n:pos;
+        return cnt;
     }
 };
