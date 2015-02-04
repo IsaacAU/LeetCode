5fa498064053a398b4941aad00d08d858229718d
Isaac <ryanwoo1989@gmail.com>
2015-02-03 22:26:34 -0500

    Update Find Peak Element.cpp

--- codes/Find Peak Element.cpp
+++ codes/Find Peak Element.cpp
@@ -1,3 +1,4 @@
+// O(n) solution
 class Solution {
 public:
     int findPeakElement(const vector<int> &num) {
@@ -8,5 +9,21 @@ public:
             ++idx;
         }
         return idx;
+    }
+};
+
+// O(logn) solution (Binary search)
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) {
+        int i=0, j=num.size()-1;
+        while(i<j){
+            int m=(i+j)/2;
+            if(m==num.size()-1 || num[m]>num[m+1])
+                j=m;
+            else
+                i=m+1;
+        }
+        return i;
     }
 };
